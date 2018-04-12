bc8e7a8bf069cfe13b9344e296fd7f45afbfa275
Chris Parr <ccparr13@gmail.com>
2018-04-12 14:01:07 +0100

    Fixed bug where brakes fail to release when the brake key is released + applied rotation and steering to the wheel objects so they roll when moving and pitch when steering

--- Assets/_Scripts/VehicleController.cs
+++ Assets/_Scripts/VehicleController.cs
@@ -32,8 +32,7 @@ namespace drivetime.vehicles
 			m_carRigidBody = GetComponent<Rigidbody>();
 			m_carRigidBody.centerOfMass = m_centerOfMass.transform.localPosition;
 		}
-
-		// Update is called once per frame
+			
 		private void FixedUpdate ()
 		{
 			m_power = 0f;
@@ -46,24 +45,48 @@ namespace drivetime.vehicles
 			m_axles[0].leftWheelCollider.steerAngle = m_steer;
 			m_axles[0].rightWheelCollider.steerAngle = m_steer;
 
-			if (Input.GetKey(KeyCode.Space))
+
+			if (Input.GetKey (KeyCode.Space))		//engage brakes
 			{
-				m_axles[1].leftWheelCollider.motorTorque = 0f;
-				m_axles[1].rightWheelCollider.motorTorque = 0f;
+				m_axles [1].leftWheelCollider.motorTorque = 0f;
+				m_axles [1].rightWheelCollider.motorTorque = 0f;
 
 				m_brake = m_carRigidBody.mass * m_brakingCoefficient;
 
-				m_axles[0].leftWheelCollider.brakeTorque = m_brake;
-				m_axles[0].rightWheelCollider.brakeTorque = m_brake;
-				m_axles[1].leftWheelCollider.brakeTorque = m_brake;
-				m_axles[1].rightWheelCollider.brakeTorque = m_brake;
+				m_axles [0].leftWheelCollider.brakeTorque = m_brake;
+				m_axles [0].rightWheelCollider.brakeTorque = m_brake;
+				m_axles [1].leftWheelCollider.brakeTorque = m_brake;
+				m_axles [1].rightWheelCollider.brakeTorque = m_brake;
+			}
+			else
+			if (Input.GetKeyUp (KeyCode.Space))		//release brakes
+			{
+				m_axles [0].leftWheelCollider.brakeTorque = 0f;
+				m_axles [0].rightWheelCollider.brakeTorque = 0f;
+				m_axles [1].leftWheelCollider.brakeTorque = 0f;
+				m_axles [1].rightWheelCollider.brakeTorque = 0f;
 			}
 			else
 			{
 				m_axles[1].leftWheelCollider.motorTorque = m_power;
 				m_axles[1].rightWheelCollider.motorTorque = m_power;
 			}
+			SetWheelTransforms ();
+		}
 
+		private void SetWheelTransforms()
+		{
+			foreach (Axle axl in m_axles)
+			{
+				Vector3 position;
+				Quaternion rotation;
+				//left
+				axl.leftWheelCollider.GetWorldPose(out position, out rotation);
+				axl.leftWheel.transform.SetPositionAndRotation (position, rotation);
+				//right
+				axl.rightWheelCollider.GetWorldPose(out position, out rotation);
+				axl.rightWheel.transform.SetPositionAndRotation (position, rotation);
+			}
 		}
 	}
 }
