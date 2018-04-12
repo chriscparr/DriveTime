acfa6f8850478019a99299c43ff30bf584275dca
Chris Parr <ccparr13@gmail.com>
2018-04-12 12:07:27 +0100

    Created simple vehicle controller and configured it for use with our car. Car can now accelerate, steer and brake around the test area!

---
+++ Assets/_Scripts/Axle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace drivetime.vehicles
+{
+	[System.Serializable]
+	public class Axle
+	{
+		public GameObject leftWheel;
+		public GameObject rightWheel;
+		public WheelCollider leftWheelCollider;
+		public WheelCollider rightWheelCollider;
+		public bool applyMotor;
+		public bool applySteering;
+	}
+}
