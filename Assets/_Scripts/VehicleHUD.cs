b138a907db1452bd5a013570af725be7e3934e99
Chris Parr <ccparr13@gmail.com>
2018-04-17 10:18:16 +0100

    Created really simple hud - currently only speed is working. position will be implemented when the other racers are ready

---
+++ Assets/_Scripts/VehicleHUD.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace drivetime.vehicles
+{
+	public class VehicleHUD : MonoBehaviour 
+	{
+		[SerializeField]
+		private Text m_positionText;
+		[SerializeField]
+		private Text m_speedText;
+		
+		public void UpdateSpeedText(float a_speed)
+		{
+			int rounded = Mathf.RoundToInt (a_speed);
+			m_speedText.text = rounded.ToString() + " MPH";
+		}
+		
+		public void UpdatePositionText(int a_position)
+		{
+			//later...
+		}
+		
+	}
+
+}
+
+
