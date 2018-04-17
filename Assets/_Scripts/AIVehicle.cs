161b84b42b85bca70946004a094041d655c14d5c
Chris Parr <ccparr13@gmail.com>
2018-04-17 16:26:49 +0100

    Implemented alternative solution for controlling vehicles with AI - navAgent as child using desiredVelocity to calculate axis inputs. This produces more realistic car behaviour.

--- Assets/_Scripts/AIVehicle.cs
+++ Assets/_Scripts/AIVehicle.cs
@@ -12,8 +12,36 @@ namespace drivetime.vehicles
 		[SerializeField]
 		private NavMeshAgent m_agent;
 
+		[SerializeField]
+		private Axle[] m_axles;
+		[SerializeField]
+		private float m_enginePower = 150f;
+		[SerializeField]
+		private float m_steerMaxAngle = 50f;
+		[SerializeField]
+		private GameObject m_centerOfMass;
+		[SerializeField]
+		private float m_brakingCoefficient = 0.5f;
+		[SerializeField]
+		private MeshCollider m_carCollider;
+
+		private Rigidbody m_carRigidBody;
+		private Transform m_agentTransform;
 		private int m_waypointIndex = 0;
 		private Vector3[] m_waypoints;
+
+		private float m_power;
+		private float m_brake;
+		private float m_steer;
+
+		private void Awake()
+		{
+			m_carRigidBody = GetComponent<Rigidbody>();
+			m_carRigidBody.centerOfMass = m_centerOfMass.transform.localPosition;
+			m_agentTransform = m_agent.gameObject.transform;
+			m_agentTransform.localPosition = Vector3.zero;
+			m_agentTransform.localRotation = Quaternion.Euler (Vector3.zero);
+		}
 
 		private void Start()
 		{
@@ -21,9 +49,84 @@ namespace drivetime.vehicles
 			m_agent.destination = m_waypoints [0];
 		}
 
+		private void FixedUpdate ()
+		{
+			m_power = 0f;
+			m_brake = 0f;
+			m_steer = 0f;
+
+			Vector3 navDirection = transform.InverseTransformDirection(m_agent.desiredVelocity);			
+
+
+			float horizontalNav = Mathf.Clamp (navDirection.x, -1f, 1f);
+			float verticalNav = Mathf.Clamp (navDirection.z, -1f, 1f);
+
+			m_agentTransform.localPosition = Vector3.zero;
+			m_agentTransform.localRotation = Quaternion.Euler (Vector3.zero);
+
+			m_power = verticalNav * m_enginePower * Time.fixedDeltaTime * 250f;
+			m_steer = horizontalNav * m_steerMaxAngle;
+
+			m_axles[0].leftWheelCollider.steerAngle = m_steer;
+			m_axles[0].rightWheelCollider.steerAngle = m_steer;
+
+			m_axles[1].leftWheelCollider.motorTorque = m_power;
+			m_axles[1].rightWheelCollider.motorTorque = m_power;
+
+			SetWheelTransforms ();
+
+			if (m_agent.remainingDistance < 3f)
+			{
+				GotoNextPoint();
+			}
+			/*
+			if (Input.GetKey (KeyCode.Space))		//engage brakes
+			{
+				m_axles [1].leftWheelCollider.motorTorque = 0f;
+				m_axles [1].rightWheelCollider.motorTorque = 0f;
+
+				m_brake = m_carRigidBody.mass * m_brakingCoefficient;
+
+				m_axles [0].leftWheelCollider.brakeTorque = m_brake;
+				m_axles [0].rightWheelCollider.brakeTorque = m_brake;
+				m_axles [1].leftWheelCollider.brakeTorque = m_brake;
+				m_axles [1].rightWheelCollider.brakeTorque = m_brake;
+			}
+			else
+				if (Input.GetKeyUp (KeyCode.Space))		//release brakes
+				{
+					m_axles [0].leftWheelCollider.brakeTorque = 0f;
+					m_axles [0].rightWheelCollider.brakeTorque = 0f;
+					m_axles [1].leftWheelCollider.brakeTorque = 0f;
+					m_axles [1].rightWheelCollider.brakeTorque = 0f;
+				}
+				else
+				{
+					m_axles[1].leftWheelCollider.motorTorque = m_power;
+					m_axles[1].rightWheelCollider.motorTorque = m_power;
+				}
+				*/
+		}
+
+		private void SetWheelTransforms()
+		{
+			foreach (Axle axl in m_axles)
+			{
+				Vector3 position;
+				Quaternion rotation;
+				//left
+				axl.leftWheelCollider.GetWorldPose(out position, out rotation);
+				axl.leftWheel.transform.SetPositionAndRotation (position, rotation);
+				//right
+				axl.rightWheelCollider.GetWorldPose(out position, out rotation);
+				axl.rightWheel.transform.SetPositionAndRotation (position, rotation);
+			}
+		}
+
 		private void GotoNextPoint()
 		{
 			m_waypointIndex++;
+			Debug.Log ("Now heading to waypoint " + m_waypointIndex.ToString());
 			if (m_waypointIndex < m_waypoints.Length)
 			{
 				m_agent.destination = m_waypoints[m_waypointIndex];
@@ -32,14 +135,6 @@ namespace drivetime.vehicles
 			{
 				m_waypointIndex = 0;
 				m_agent.destination = m_waypoints[m_waypointIndex];
-			}
-		}
-
-		private void Update()
-		{
-			if (m_agent.remainingDistance < 2f)
-			{
-				GotoNextPoint();
 			}
 		}
 	}
