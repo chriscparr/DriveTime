ba2864085a21d5883deeb9f0ecbf6f7e61d2b68d
Chris Parr <ccparr13@gmail.com>
2018-04-17 12:15:33 +0100

    Script to allow access to the waypoints setup for this track

---
+++ Assets/_Scripts/BankedTrack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace drivetime
+{
+	public class BankedTrack : MonoBehaviour 
+	{
+		[SerializeField]
+		private GameObject[] m_waypointObjects;
+		
+		private Vector3[] m_waypoints;
+		
+		public Vector3[] Waypoints {get{ return m_waypoints;}}
+		
+		private void Awake()
+		{
+			m_waypoints = new Vector3[m_waypointObjects.Length];
+			for (int i = 0; i < m_waypointObjects.Length; i++)
+			{
+				m_waypoints [i] = m_waypointObjects [i].transform.position;
+			}
+		}
+	}
+}
